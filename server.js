c01831c761df352cd1f4229605086341c430f14d
Tim Hostetler <thostetler@users.noreply.github.com>
2019-06-11 14:23:37 -0400

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Vision = require('@hapi/vision');
 const Ejs = require('ejs');
 const http2 = require('http2');
 const api = require('./lib/api');
+const responseParser = require('./lib/responseParser');
 
 const options = {
   key: fs.readFileSync('./localhost-privkey.pem'),
@@ -48,13 +49,13 @@ const init = async () => {
         return h.redirect('/');
       }
       const response = await api.search(request.query);
-      console.log(response);
+      console.log(response.response.docs[0]);
 
       if (!response.response) {
         return h.redirect('/');
       }
 
-      const { numFound, docs } = response.response;
+      const { numFound, docs } = responseParser.parse(response.response);
       return h.view('search', {
         query: request.query,
         title: request.query.q + ' | Search',
